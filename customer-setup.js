8a07aec458453ac419178a6ad63e4a32504f6448
tleach01 <taylorleach0@gmail.com>
2020-08-25 19:23:40 -0400

    changed for list to div

--- customer-setup.js
+++ customer-setup.js
@@ -2,20 +2,22 @@
 function addPerson () {
   const customerProfile = document.querySelector('#customerInfo')
   for (let customer of customers) {
-    const li = document.createElement('li')
-    customerProfile.appendChild(li)
-    li.setAttribute('class', 'person-li')
-    const person = document.querySelector('.person-li')
+    const div = document.createElement('div')
+    customerProfile.appendChild(div)
+    const person = document.querySelector('.person-div')
+
     const img = document.createElement('img')
     img.src = customer.picture.large
     customerProfile.lastElementChild.appendChild(img)
+
     const h2 = document.createElement('h2')
     customerProfile.lastElementChild.appendChild(h2)
     h2.innerText = customer.name.first + ' ' + customer.name.last
     // need to capitalize first letters
-    const p = document.createElement('p')
-    customerProfile.lastElementChild.appendChild(p)
-    p.innerText = customer.email
+
+    const email = document.createElement('p')
+    customerProfile.lastElementChild.appendChild(email)
+    email.innerText = customer.email
 }
 }
 addPerson();
